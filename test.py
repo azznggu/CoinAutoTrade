b4ee2996534ec6ffa95886f06bf57dee2cf93559
Jong Un Park <azznggu@gmail.com>
2021-05-14 16:30:55 +0900

    수정

--- test.py
+++ test.py
@@ -1,40 +1,8 @@
-import json
-import requests
 import pyupbit
 
-access = "Ri8sxlJg25hn91uuMdxxdSu7nDV4TwaEJxfsU1Fp"
-secret = "NnevoGtx8ySsTP3PXBxg2GU5X"
-slack_token = "xoxb-2064130842913-2036913322215-iJgkwYNbHpjAKaK6bI0KMx0q"
-channel = "#develop"
-
+access = "Ri8sxlJg25hn91uuMdxxdSu7nDV4TwaEJxfsU1Fp"          # 본인 값으로 변경
+secret = "NnevoGtx8ySsTP3PXBxg2GU5XmMpT204mDF16AVd"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
-print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
-
-def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
-    response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text}
-    )
-
-def get_balance(coin):
-    """잔고 조회"""
-    balances = upbit.get_balances()
-    for b in balances:
-        if b['currency'] == coin:
-            if b['balance'] is not None:
-                return float(b['balance'])
-            else:
-                return 0
-upbit = pyupbit.Upbit(access, secret)
 print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회    
-
-count = 1
-post_message(slack_token, channel, "autotrade start")
-while count > 0:
-    post_message(slack_token, channel, "message test" + str(count) + str(upbit.get_balance("KRW")))
-    count = count-1
-    
